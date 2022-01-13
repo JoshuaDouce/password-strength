c74d8af7e44ce584a34755bf0d10eff1edce7854
JoshuaDouce <Joshuadouce@gmail.com>
2022-01-13 10:46:24 +0000

    UI Elements added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  Card,
+  Box,
+  CardContent,
+  Typography,
+  TextField,
+  Checkbox,
+  FormControlLabel,
+  FormControl,
+  FormLabel,
+  FormHelperText
+} from '@mui/material';
+import { RadioButtonUnchecked, RadioButtonChecked } from '@mui/icons-material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ minWidth: 300 }}>
+      <Card variant="outlined">
+        <>
+          <CardContent>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                flexDirection: 'column',
+                m: '1em 0'
+              }}>
+              <Typography variant="h4">Create an account</Typography>
+              <Typography variant="subtitle1">Continue to Site</Typography>
+            </Box>
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <FormControl sx={{ ml: 3 }} component="fieldset" variant="standard" error={true}>
+                <TextField
+                  sx={{ marginBottom: '1em' }}
+                  label="Password"
+                  color="primary"
+                  error={true}
+                />
+                <TextField label="ConfirmPassword" color="primary" error={true} />
+                <FormHelperText sx={{ fontWeight: 'bold' }}>Passwords do not match</FormHelperText>
+              </FormControl>
+            </Box>
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <FormControl sx={{ ml: 3, mt: 1 }} component="fieldset" variant="standard">
+                <FormLabel component="legend">Password Strength</FormLabel>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      color="success"
+                      icon={<RadioButtonUnchecked />}
+                      checkedIcon={<RadioButtonChecked />}
+                      disabled
+                    />
+                  }
+                  label="Upper case character"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      color="success"
+                      icon={<RadioButtonUnchecked />}
+                      checkedIcon={<RadioButtonChecked />}
+                      disabled
+                    />
+                  }
+                  label="Lower case character"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      color="success"
+                      icon={<RadioButtonUnchecked />}
+                      checkedIcon={<RadioButtonChecked />}
+                      disabled
+                    />
+                  }
+                  label="Non alphanumeric character eg @"
+                />
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      color="success"
+                      icon={<RadioButtonUnchecked />}
+                      checkedIcon={<RadioButtonChecked />}
+                      disabled
+                    />
+                  }
+                  label="Longer than 7 characters"
+                />
+              </FormControl>
+            </Box>
+          </CardContent>
+        </>
+      </Card>
+    </Box>
   );
 }
 
