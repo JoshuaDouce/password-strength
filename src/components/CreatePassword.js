dba517b0132b59edb03aeb07dafee7bb787a3034
JoshuaDouce <Joshuadouce@gmail.com>
2022-01-19 08:27:20 +0000

    Remove completed todo comment

--- src/components/CreatePassword.js
+++ src/components/CreatePassword.js
@@ -4,10 +4,6 @@ import CardHeader from './CardHeader';
 import PasswordConfirmation from './PasswordConfirmation';
 
 const CreatePassword = () => {
-  /**
-   * TODO: Add a delay to error messages showing until the user has stopped typing
-   */
-
   return (
     <Box sx={{ display: 'flex', justifyContent: 'center', m: '1em', minWidth: 300 }}>
       <Card sx={{ m: '1em' }} variant="outlined">
